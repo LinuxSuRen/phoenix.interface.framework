79b12b8c05a94512f345fc74d0efc472ca7b3379
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2016-08-14 03:52:08 +0000

    增加json的校验

---
+++ src/main/java/org/suren/autotest/interfaces/framework/json/JsonExpectExtend.java
@@ -0,0 +1,13 @@
+/**
+ * http://surenpi.com
+ */
+package org.suren.autotest.interfaces.framework.json;
+
+/**
+ * @author suren
+ * @date Aug 14, 2016 11:44:29 AM
+ */
+public interface JsonExpectExtend
+{
+	boolean expect(String jsonStr);
+}
